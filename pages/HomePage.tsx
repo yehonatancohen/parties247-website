3cbb720eee73df558de0a5a31f8ef9452e21c132
Yehonatan Cohen <57250744+yehonatancohen@users.noreply.github.com>
2025-11-23 20:02:28 +0200

    Fix hero poster data URL formatting

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -8,8 +8,8 @@ import SocialsCta from '../components/SocialsCta';
 import { BASE_URL, SOCIAL_LINKS } from '../constants';
 import { createCarouselSlug } from '../lib/carousels';
 
-const HERO_POSTER_DATA_URL = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAP//////////////////////////////////////////////////////////////////////////////////////2wBDAf//////////////////////////////////////////////////////////////////////wAARCAABAAEDAREAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEB
-AQAAAAAAAAAAAAAAAAAAAgP/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCf/9k=';
+const HERO_POSTER_DATA_URL =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=';
 
 const HomePage: React.FC = () => {
   const { parties, carousels, isLoading, error, loadingMessage } = useParties();
