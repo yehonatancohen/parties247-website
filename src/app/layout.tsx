fb1ccafad8236d0221adbc2f8c29f5ddf3f0edee
yehonatancohen <yoncohenyon@gmail.com>
2026-02-10 00:09:46 +0200

    Last try on this pixel thing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import SwiperRegister from "../components/SwiperRegister";
 import { Assistant, Rubik } from "next/font/google";
 import { BASE_URL, BRAND_LOGO_URL } from "@/data/constants";
 import { Suspense } from "react";
+import Script from "next/script";
 
 // Component Imports
 import Header from '../components/Header';
@@ -68,6 +69,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="he" dir="rtl" className={`${assistant.variable} ${rubik.variable}`}>
       <body suppressHydrationWarning className="font-sans">
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+               !function(f,b,e,v,n,t,s)
+               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+               n.queue=[];t=b.createElement(e);t.async=!0;
+               t.src=v;s=b.getElementsByTagName(e)[0];
+               s.parentNode.insertBefore(t,s)}(window, document,'script',
+               'https://connect.facebook.net/en_US/fbevents.js');
+               fbq('init', '855037240866987');
+               fbq('track', 'PageView');
+             `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=855037240866987&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+
         <Suspense fallback={null}>
           <MetaPixelEvents />
         </Suspense>
